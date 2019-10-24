24209ff1bc976e0b5a7b9ee154294e17f088afb4
Abram Hindle <abram.hindle@softwareprocess.es>
2019-10-24 01:03:11 -0600

    passes the tests

--- endlesssky.py
+++ endlesssky.py
@@ -49,12 +49,10 @@ def count_init_tabs(s):
 
 def parse_entity(line,lines,depth=0):
     ek, ev = parse_tuple(line)
-    print(ek,ev)
     lines = lines[1:]
     tabs = "\t"*(depth+1)
     entity = [(ek,ev)]
     while( len(lines) > 0 ):
-        print(entity)
         c = count_init_tabs(lines[0])
         if c == depth+2:
             sentity, rlines = parse_entity(line,lines,depth=depth+1)
@@ -71,15 +69,61 @@ def parse_entity(line,lines,depth=0):
             return (entity, lines)
     return (entity, lines)
 
-if __name__ == "__main__":
+def get_name(x):
+    if isinstance(x,list):
+        return x[0][0]
+    else:
+        return x[0]
+
+def get_value(x):
+    if isinstance(x,list):
+        return x[0][1]
+    else:
+        return x[1]
+def get_properties(x):
+    if isinstance(x,list):
+        return x[1:]
+    return None
+
+def mk_val(entity, val):
+    if isinstance(entity,list):
+        entity[0][1] = val
+        return entity
+    else:
+        return (entity[0], val)
+    
+def endless_type_grep(entities, stype):
+    return [x for x in entities if get_name(x) == stype]
+
+def endless_name_grep(entities, name):
+    return [x for x in entities if get_value(x) == name]
+
+def endless_has(entity, entry):
+    return len(endless_type_grep(entity, entry)) > 0
+
+def endless_replace(entity, prop, val):
+    """ mutates! """
+    for i in range(0,len(entity)):
+        if get_name(entity[i]) == prop:
+            entity[i] = mk_val(entity[i], val)
+            break
+    return entity
+
+def endless_first(entity, stype):
+    return endless_type_grep(entity, stype)[0]
+
+def test_driver():
     maps = parse_endless_sky_file("data.orig/map.txt")
-    print(maps)
     planets = endless_type_grep(maps, "planet")
-    earth = endless_name_grep(planets, "Earth")
+    assert(len(planets) > 0)
+    earth = endless_name_grep(planets, "Earth")[0]
     assert(endless_has(earth, "landscape"))
     assert(endless_has(earth, "description"))
     assert(endless_has(earth, "bribe"))
-    assert(endless_has(earth, "required reputation"))
+    assert(endless_has(earth, '"required reputation"'))
     earth = endless_replace( earth, "bribe", 0.1)
     bribe = endless_first(earth, "bribe")
-    assert bribe == 0.1
+    assert bribe[1] == 0.1
+
+if __name__ == "__main__":
+    test_driver()
