91c806d57a6f00217e33cf39543a5b10da75ba31
Abram Hindle <abram.hindle@softwareprocess.es>
2019-10-27 23:16:20 -0600

    neighborhood heuristic seems to work

--- graph-map.py
+++ graph-map.py
@@ -43,7 +43,7 @@ pos = forceatlas2.forceatlas2_networkx_layout(G, pos=pos,
     gravity=1.0)
 nx.set_node_attributes(G, pos, 'pos')
 
-def graphit(G):
+def graphit(G, govt=None):
     pos = nx.get_node_attributes(G, 'pos')
     # find node near center (0.5,0.5)
     dmin = 1
@@ -56,12 +56,22 @@ def graphit(G):
             dmin = d
 
     # color by path length from node near center
-    p = dict(nx.single_source_shortest_path_length(G, ncenter))
+    if govt is None:
+        govt = dict([(n,1) for n in G.nodes ])
+    print(govt)
+    govts = list(set(govt.values()))
+    govts.sort()
+    govt_to_color = dict(zip(govts, range(len(govts))))
+    colors = [govt_to_color[govt[n]] for n in G.nodes]
+    print(govts)
+    print(colors)
+    # p = dict(nx.single_source_shortest_path_length(G, ncenter))
     plt.figure(figsize=(8, 8))
     nx.draw_networkx_edges(G, pos)
     nx.draw_networkx_nodes(G, pos,
-                        node_size=80,
-                        cmap=plt.cm.Reds_r)
+                           node_color=colors,
+                           node_size=80,
+                           cmap=plt.cm.tab10_r)
     plt.axis('off')
     plt.show()
 
@@ -77,7 +87,73 @@ random.shuffle(systems)
 nodes = list(G.nodes())
 random.shuffle(nodes)
 
-node_to_system = dict(zip(nodes,systems))
+# at this point the association is complete
+system_names = [system[0][1] for system in systems]
+system_names_to_system = dict(zip(system_names, systems))
+governments = [es.endless_first(system, "government")[1] for system in systems]
+system_names_to_governments = dict(zip(system_names, governments))
+node_to_system_names = dict(zip(nodes,system_names))
+
+def swap_node(G, node1, node2):
+    """ this is a lot of work """
+    pos = nx.get_node_attributes(G, 'pos')
+    neighbors1 = G.neighbors(node1)
+    neighbors2 = G.neighbors(node2)
+    # remove the edges
+    for n1 in neighbors1:
+        G.remove_edge(node1,n1)
+    for n2 in neighbors2:
+        G.remove_edge(node2,n2)
+    # add the remove nodes
+    for n1 in neighbors1:
+        G.add_edge(node2,n1)
+    for n2 in neighbors2:
+        G.add_edge(node1,n2)
+    # swap the pos
+    pos[node1], pos[node2] = pos[node2], pos[node1]
+    nx.set_node_attributes(G, 'pos', pos)
+
+def shuffle_of(l):
+    ll = list(l)
+    random.shuffle(ll)
+    return ll
+    
+def dictswap(dict1, node1,node2):
+    dict1[node1], dict1[node2] = dict1[node2], dict1[node1]
+    
+def swap_systems(node1, node2):
+    dictswap(node_to_system_names, node1, node2)
+
+def govt_of_node(node):
+    system_name = node_to_system_names[node]
+    return system_names_to_governments[system_name]
+    
+# idea: given a node can we increase the government adjacent count of its neighbors?
+def count_govt_neighbor(node):
+    govt = govt_of_node(node)
+    x = sum([govt_of_node(n) == govt for n in G.neighbors(node)])
+    return (x, govt)
+
+def keep_neighborhoods_similar():
+    # keep the neighborhoods kinda similar
+    for node in shuffle_of(nodes):
+        system_name = node_to_system_names[node]
+        count, govt = count_govt_neighbor( node )
+        other_govts = [g for g in [(govt_of_node(n),n) for n in G.neighbors(node)] if g[0] != govt]
+        for gn in other_govts:
+            g,n = gn
+            swap_systems( node, n )
+            c2, _ = count_govt_neighbor( node )
+            if (c2 < count):
+                """ undo """
+                swap_systems( node, n )
+
+for i in range(100):
+    keep_neighborhoods_similar()
+
+
+
+
 
 def move_system(system, x, y):
     system = es.endless_replace( system, 'pos', (x,y) )
@@ -86,12 +162,11 @@ def move_system(system, x, y):
 def remove_links(system):
     return es.endless_delete_type(system, 'link')
 
-i = 0
 pos = nx.get_node_attributes(G, 'pos')
+node_to_system = dict([ (n,system_names_to_system[ node_to_system_names[n] ])  for n in node_to_system_names ])
 
-for i in range(len(systems)):
-    system = systems[i]
-    node = nodes[i]
+for node in node_to_system:
+    system = node_to_system[node]
     x = pos[node][0]
     y = pos[node][1]
     xp = x/60 * width - minx
@@ -100,7 +175,6 @@ for i in range(len(systems)):
     system = remove_links( system )
     for neighbor in G.neighbors(node):
         es.endless_add_property(system,('link', node_to_system[neighbor][0][1]))
-    i += 1
     node_to_system[node] = system
     
 for k in node_to_system:
@@ -110,4 +184,6 @@ for k in node_to_system:
 out = es.serialize_entities(maps)
 open("maps.txt.out", 'w').write(out)
 
-graphit(G)
+node_to_govt = dict([(n, system_names_to_governments[ node_to_system_names[ n ] ]) for n in node_to_system_names])
+
+graphit(G, govt=node_to_govt)
